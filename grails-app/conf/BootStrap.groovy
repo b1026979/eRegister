7b49d9a61d842449db1b7e6e2b35689d2d1441ae
b1026979 <b1026979@my.shu.ac.uk>
2013-04-20 18:22:16 +0100

    added the automatic class filling to BotStrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,6 +7,10 @@ class BootStrap {
 	def ian_instructor = Instructor.findByStaffid('646345d') ?: new Instructor(staffid:'646345d', name:'Ian Ibbotson').save()
 	def mathew_instructor = Instructor.findByStaffid('253425') ?: new Instructor(staffid:'253425', name:'Mathew Love').save()
 
+	def web_arch_course = Course.findByCourseCode('443343dd') ?: new Course(courseCode:'443343dd',
+                                                                                courseName:'Web Archs',
+                                                                                courseDescription:'my optional choise').save()
+
     }
     def destroy = {
     }
