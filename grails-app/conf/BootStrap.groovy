5cbf1ac976b489670cfca69b32fe53ba6ee32b3f
b1026979 <b1026979@my.shu.ac.uk>
2013-04-20 17:59:15 +0100

    bootStrap completed by making automated producing instructors

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,12 @@
+import uk.ac.shu.webarch.eregister.*;
 class BootStrap {
 
     def init = { servletContext ->
+	println("BootStrap::init");
+
+	def ian_instructor = Instructor.findByStaffid('646345d') ?: new Instructor(staffid:'646345d', name:'Ian Ibbotson').save()
+	def mathew_instructor = Instructor.findByStaffid('253425') ?: new Instructor(staffid:'253425', name:'Mathew Love').save()
+
     }
     def destroy = {
     }
