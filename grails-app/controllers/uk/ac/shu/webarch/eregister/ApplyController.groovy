44a91e83cf486f2761ebb1eaa64f7811faf78978
b1026979 <b1026979@my.shu.ac.uk>
2013-05-05 11:09:48 +0100

    Finished apply  with index and proccessAttendance

--- grails-app/controllers/uk/ac/shu/webarch/eregister/ApplyController.groovy
+++ grails-app/controllers/uk/ac/shu/webarch/eregister/ApplyController.groovy
@@ -5,38 +5,26 @@ import grails.converters.*
   class ApplyController {
 
     def index() {
-/*
-      def result=[:]
-        result.attendanceList= []
-        
-        Student.findAll() each { st ->
-          result.attendanceList.add([Attendance:st.studentAttended,
-                                     timeStamp:regentry.now])
-        }
 
-      withFormat {
-         html result
-         xml { render result as XML }
-         json { render result as JSON }
-      }
-*/
-  }
-    def processAttendance() {
+
+    }
+
+  def processAttendance() {
 
    println(params)
    println("Student id is ${params.StudentId}");
-   println("Register entry id is ${params.RegistrationSheet}");
+   println("Register entry id is ${params.RegistrationId}");
 
-   def student_a = Student.findByStudentNumber(params.StudentId)
+   def student_a = Student.findByStudentNumber(params.StudentId) ?:
+   println("Error has accurred")
 
    def regSheet_a = RegistrationSheet.findByRegSheetNumber(params.RegistrationId)
+   def new_reg_entry = new RegisterEntry(student:student_a,regSheetNumber:regSheet_a).save()
 
    println("Looked up student is ${student_a}, sheet is ${regSheet_a}");
 
-
-
-   redirect(action:'index')
-   
   
-     }
+   redirect(action:'index', params:params)
+
+  }
 }
